b74847af913778921e44e95406bc56f6a8669b6c
jihwanAn <an0572@naver.com>
2024-04-05 03:28:08 +0900

    [Client] button 컴포넌트 작성

---
+++ client/src/components/button.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  border: none;
+  display: inline-block;
+  padding: 10px 20px;
+  font-weight: bold;
+  border-radius: 5px;
+  transition: background-color 0.8s;
+  background-color: #459cff;
+  color: #fff;
+  transform: translateY(1px);
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const TextButton = styled(Button)`
+  margin: 0;
+  padding: 0 10px;
+  border: none;
+  background: none;
+  color: black;
+  font-size: ${(props) => props.size || "15px"};
+
+  &:hover {
+    color: #0056b3;
+    background: none;
+  }
+`;
+
+const StyledBtn = ({ children, className, onClick }) => (
+  <Button className={className} onClick={onClick}>
+    {children}
+  </Button>
+);
+
+const TextStyledButton = ({ className, onClick, children, size }) => (
+  <TextButton className={className} onClick={onClick} size={size}>
+    {children}
+  </TextButton>
+);
+
+export { StyledBtn, TextStyledButton };
